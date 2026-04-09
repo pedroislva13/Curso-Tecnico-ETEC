1951e97b0d6e953483173817511e8ee848859582
pedroislva13 <henriquesilvadeoliveirap46@gmail.com>
2026-04-09 18:38:56 -0300

    Delete l1.ex6.py

--- l1.ex6.py
+++
@@ -1,6 +0,0 @@
-distancia = float(input("Digite a distância a percorrer (em km): "))
-velocidade = float(input("Digite a velocidade média esperada (em km/h): "))
-
-tempo = distancia / velocidade
-
-print(f"O tempo estimado de viagem é de {tempo:.2f} horas.")
