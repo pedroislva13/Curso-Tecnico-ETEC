561b6e3b225dbdecf8d7fb3d28d0c7b1e4c30815
pedroislva13 <henriquesilvadeoliveirap46@gmail.com>
2026-04-09 20:33:35 -0300

    Update l2.ex8.py

--- l2.ex8.py
+++ l2.ex8.py
@@ -1,16 +1,14 @@
-import math
+ area = float(input("Digite o tamanho em metros quadrados (m²) da área a ser pintada: "))
 
-area = float(input("Digite o tamanho em metros quadrados (m²) da área a ser pintada: "))
+litros = area / 3.0
 
-litros_necessarios = area / 3.0
+lata = 18.0
+preco = 80.00
 
-capacidade_lata = 18.0
-preco_lata = 80.00
+quantidade = (litros / lata)
 
-quantidade_latas = math.ceil(litros_necessarios / capacidade_lata)
+preco = quantidade * preco
 
-preco_total = quantidade_latas * preco_lata
-
-print(f"\nPara pintar {area} m², você precisará de {litros_necessarios:.2f} litros de tinta.")
-print(f"Quantidade de latas a comprar: {quantidade_latas}")
-print(f"Preço total: R$ {preco_total:.2f}")
+print(f"Para pintar {area} m², você precisará de {litros} litros de tinta.")
+print(f"Quantidade de latas a comprar: {quantidade}")
+print(f"Preço total: R$ {preco}")
