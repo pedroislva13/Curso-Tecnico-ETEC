eea9510cefc7f52602b5b9008309c05fe715064a
pedroislva13 <henriquesilvadeoliveirap46@gmail.com>
2026-04-09 18:47:00 -0300

    Add files via upload

---
+++ l1.ex6.py
@@ -0,0 +1,6 @@
+distancia = float(input("Digite a distância a percorrer (em km): "))
+velocidade = float(input("Digite a velocidade média esperada (em km/h): "))
+
+tempo = distancia / velocidade
+
+print(f"O tempo estimado de viagem é de {tempo:.2f} horas.")
---
+++ l1.ex8.py
@@ -0,0 +1,4 @@
+fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(f"A temperatura em Celsius é: {celsius:.1f}°C")
