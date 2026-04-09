d9447f74c9350dec888e589d0151fae24a952ca1
pedroislva13 <henriquesilvadeoliveirap46@gmail.com>
2026-04-09 20:08:48 -0300

    Update l1.ex1.py

--- l1.ex1.py
+++ l1.ex1.py
@@ -1,5 +1,3 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> n1 = int(input("Digite o primeiro número inteiro: "))
 ... n2 = int(input("Digite o segundo número inteiro: "))
 ... soma = n1 + n2
